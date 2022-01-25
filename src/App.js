2a8606c8bf88897af56c2e95a570de7ddbadb33d
Sawar-Nabi <sawarnabi100@gmail.com>
2022-01-25 15:24:23 +0530

    new file added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from "react";
 import "./styles/style.css";
-import { Constant } from "./constants";
-import styled from "styled-components";
-const { Fonts, Colors } = Constant;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Section>
-      {console.log(Colors)}
-      <h1>hello</h1>
-    </Section>
+    <BrowserRouter>
+      <h1>Hello</h1>
+      <h1>Google</h1>
+    </BrowserRouter>
   );
 };
 
 export default App;
-
-const Section = styled.section`
-  h1 {
-    font-family: ${Fonts.primaryFont};
-    color: ${Colors.primaryColor};
-  }
-`;
