8188daa10460f80e42e4591e2ca8659aee394279
Sawar-Nabi <sawarnabi100@gmail.com>
2022-01-25 15:10:33 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/style.css";
+import { Constant } from "./constants";
+import styled from "styled-components";
+const { Fonts, Colors } = Constant;
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Section>
+      {console.log(Colors)}
+      <h1>hello</h1>
+    </Section>
   );
-}
+};
 
 export default App;
+
+const Section = styled.section`
+  h1 {
+    font-family: ${Fonts.primaryFont};
+    color: ${Colors.primaryColor};
+  }
+`;
---
+++ src/constants/Colors.js
@@ -0,0 +1,6 @@
+export const Colors = {
+  primaryColor: "#0A8791",
+  secondaryColor: "#FBA83C",
+  terteryColor: "#C2C2CB",
+  helperColor: "#0E122B",
+};
