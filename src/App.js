d7a19fab1c74cc45bc39ac4b9fdde3187bc76ae6
Sawar-Nabi <sawarnabi100@gmail.com>
2022-02-07 15:13:34 +0530

    Mycart Component Create

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./styles/style.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import SliderDemo from './components/SliderDemo'
+import SliderDemo from "./components/SliderDemo";
+import MyCart from "./components/MyCart";
 
 const App = () => {
   return (
     <BrowserRouter>
       {/* <Header /> */}
       <Routes>
-        <Route path='/slider' element={<SliderDemo/>} />
-        <Route path='/' element={<Header />} />
+        <Route path="/slider" element={<SliderDemo />} />
+        <Route path="/" element={<Header />} />
+        <Route path="/mycart" element={<MyCart />} />
       </Routes>
     </BrowserRouter>
   );
