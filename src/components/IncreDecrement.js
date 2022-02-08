b1ab67caf2e5ab40dad8c6f21387695f3b5c8de2
Sawar-Nabi <sawarnabi100@gmail.com>
2022-02-08 15:34:22 +0530

    MyCart component Modify

--- src/components/IncreDecrement.js
+++ src/components/IncreDecrement.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
-import { IncreDecre } from "../IncreStore";
-import MyCart from "./MyCart";
 
-const IncreDecrement = () => {
+const IncreDecrement = ({ itemFunc }) => {
   const [ counter, setCounter ] = useState(0);
 
   const increment = () => {
+    itemFunc(counter);
     setCounter(counter + 1);
   };
 
   const decrement = () => {
     if (counter == 0) {
       setCounter(0);
+      itemFunc(counter);
     } else {
+      itemFunc(counter);
       setCounter(counter - 1);
     }
   };
@@ -21,9 +22,6 @@ const IncreDecrement = () => {
       <h1>{counter}</h1>
       <button onClick={increment}>Incre</button>
       <button onClick={decrement}>Decre</button>
-      <IncreDecre.Provider value={{ counter, setCounter }}>
-        <MyCart increment={increment} decrement={decrement}/>
-      </IncreDecre.Provider>
     </div>
   );
 };
--- src/components/MyCart.js
+++ src/components/MyCart.js
@@ -1,12 +1,16 @@
-import React, { useContext } from "react";
-import { IncreDecre } from "../IncreStore";
+import React, { useState } from "react";
 import CommonNav from "./CommonNav";
 import styled from "styled-components";
 import { Constant } from "../constants";
+import IncreDecrement from "./IncreDecrement";
 const { Fonts, Colors } = Constant;
 
-const MyCart = ({ decrement, increment }) => {
-  const { counter, setCounter } = useContext(IncreDecre);
+const MyCart = () => {
+  const [ itemValue, setItemValue ] = useState(0);
+
+  const getItemValue = (value) => {
+    setItemValue(value);
+  };
 
   const example = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
   return (
@@ -14,9 +18,10 @@ const MyCart = ({ decrement, increment }) => {
       <CommonNav title="My Cart" color="#000" />
       {example.map((exam) => (
         <div>
-          {counter}
-          <button onClick={increment}>Add</button>
-          <button onClick={decrement}>Sub</button>
+          <div>{itemValue}</div>
+          15$
+          <IncreDecrement itemFunc={getItemValue} />
+          {console.log(itemValue)}
         </div>
       ))}
     </Section>
